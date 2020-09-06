15e157025ffe01864e5a2e4af4f4cb0ee56d9160
Danny Francisco <dcfran@gmail.com>
2020-09-06 09:21:48 +0800

    mi card flutter course

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:barcode_widget/barcode_widget.dart';
-import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,60 +8,87 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final logo = Image.asset(
-      "assets/logo.png",
-    );
-
-    final profile = Image.asset('assets/profile.jpg');
-
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 20, bottom: 50),
-                child: logo,
-              ),
-              CircleAvatar(
-                radius: 100.0,
-                backgroundImage: AssetImage('assets/profile.jpg'),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: LiteRollingSwitch(
-                  value: false,
-                  colorOn: Colors.black26,
-                  colorOff: Colors.black12,
-                  iconOn: Icons.lightbulb_outline,
-                  iconOff: Icons.power_settings_new,
-                  onChanged: (bool state) {
-                    print('turned ${(state) ? 'on' : 'off'}');
-                  },
+      home: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/bg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 50.0),
+                  child: CircleAvatar(
+                    radius: 60.0,
+                    backgroundImage: AssetImage('assets/profile.jpg'),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 20.0),
-                child: Text(
+                Text(
                   'Danny Francisco',
                   style: TextStyle(
+                    fontFamily: 'Pacifico',
                     fontSize: 40.0,
-                    color: Colors.black,
+                    color: Colors.blueGrey.shade400,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                child: BarcodeWidget(
-                  barcode: Barcode.code39(), // Barcode type and settings
-                  data: 'CHUCK',
-                  drawText: false,
-                  height: 100,
+                Text(
+                  'SOFTWARE DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.blue.shade300,
+                    fontSize: 25.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+63 123 456 789',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'dcfran@gmail.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro'),
+                      ),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
